998f1a8aa663e94e6496f8963acb08a960abeb38
Matt Jonker <matt@mattjonker.com>
2015-01-10 16:01:44 -0500

    Even more efficient deletion of large number of keys

--- lib/anemone/storage/redis.rb
+++ lib/anemone/storage/redis.rb
@@ -9,7 +9,7 @@ module Anemone
       def initialize(opts = {})
         @redis = ::Redis.new(opts)
         @key_prefix = opts[:key_prefix] || 'anemone'
-        @redis.del @redis.keys("#{@key_prefix}:pages:*")
+        @redis.eval("local keys = redis.call('keys', ARGV[1]) \n for i=1,#keys,5000 do \n redis.call('del', unpack(keys, i, math.min(i+4999, #keys))) \n end \n return keys", [], ["#{@key_prefix}:pages:*"])
       end
 
       def [](key)
